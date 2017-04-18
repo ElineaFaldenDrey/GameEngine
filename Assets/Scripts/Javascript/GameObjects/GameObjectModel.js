3d5f0fbd67b2a59525f154d92e134faac41bbc01
ElineaFD <auquiere.eleanore@gmail.com>
2017-04-18 11:02:34 +0200

    gameobject structure

---
+++ Assets/Scripts/Javascript/GameObjects/GameObjectModel.js
@@ -0,0 +1,27 @@
+function GameObject()
+{
+	this.name     = "Model";
+	this.enabled  = false; // est fe que l'objet est actif en ce moment 
+	this.started  = false; // a til existé au moins une foois dans la frame
+	this.renderer = true;
+
+	this.Awake = function()
+	{
+
+	};
+
+	this.Start = function()
+	{
+
+	};
+
+	this.Update = function()
+	{
+
+	};
+
+	this.GUI = function()
+	{
+
+	};
+}
